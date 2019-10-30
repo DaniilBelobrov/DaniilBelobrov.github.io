18bb69d0d1d40fdd13aab61b7f93de5dc1e1028b
DaniilBelobrov <57140273+DaniilBelobrov@users.noreply.github.com>
2019-10-30 23:07:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-
-require 'MyTest/php/Components/Head.php';
-require 'MyTest/php/Components/Body.php';
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Test</title>
+	
+</head>
+<body>
+	<p>hellp</p>
+	<script src="js/jquery-3.0.0.min.js"></script>
+	<script src="js/index.js"></script>
+</body>
+</html>
